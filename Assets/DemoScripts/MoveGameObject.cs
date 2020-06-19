e9751aa4225b8fc554819aba9e40f0f4a5a06b35
Roscoe <roscoerappaport@gmail.com>
2020-06-19 10:39:51 -0700

    Initial commit of Unity proj files

---
+++ Assets/DemoScripts/MoveGameObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MoveGameObject: MonoBehaviour
+{
+    // The speed that the GameObject will move
+    public float _speed = 1.25f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.position += (Vector3.right * _speed) * Time.deltaTime;
+        }
+        // Move the GameObject to the right this frame
+
+        if (Input.GetKey("up"))
+        {
+            transform.position += (Vector3.up * _speed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.position += (Vector3.down * _speed) * Time.deltaTime;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.position += (Vector3.left * _speed) * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/DemoScripts/MoveGameObjectAutomatic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MoveGameObjectAutomatic: MonoBehaviour
+{
+    // The speed that the GameObject will move
+    public float _speed = 3;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the GameObject to the right this frame
+        transform.position += (Vector3.right * _speed) * Time.deltaTime;
+             
+    }
+}
